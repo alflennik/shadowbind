d62e3a2ac3415c78cf8a61310ae1f8f63d90273a
Alexander Flenniken <alexander@mobyinc.com>
2017-12-10 19:54:10 -0800

    All tests (including the repeater) are passing

--- src/util/walkFragment.js
+++ src/util/walkFragment.js
@@ -3,33 +3,54 @@ import error from '../lib/error.js'
 
 // Run callback on every element in the shadowDom, applying repeaters as needed
 export default function walkFragment (component, callback) {
+  let previousNode
+
   function recursiveWalk (node) {
-    if (node.nodeType === 1) callback(node)
+    const wasAttachedToDom = node.parentNode
+    const nodeIsAnElement = node.nodeType === 1
+    if (nodeIsAnElement) callback(node)
+    const stillAttachedAfterCallback = node.parentNode
+
+    if (wasAttachedToDom && !stillAttachedAfterCallback) {
+      // Repeaters are removed by the callback, which would mess up the walk
+      return true
+    }
+
+    previousNode = node
     node = node.firstChild
+
     while (node) {
-      recursiveWalk(node)
-      node = node.nextSibling
+      const wasJustRemovedFromDom = recursiveWalk(node)
+
+      if (wasJustRemovedFromDom) {
+        node = previousNode
+      } else {
+        previousNode = node
+        node = node.nextSibling
+      }
     }
   }
 
-  if (!component.shadowRoot) {
-    try {
-      component.attachShadow({ mode: 'open' })
-    } catch (err) {
-      trace.set({ component: trace.get().component })
-      error(
-        'shadowbind_closed_shadow_root',
-        'Subscribed component has a closed shadowRoot, but only open ' +
-          'shadowRoots are supported'
-      )
-    }
-    trace.set({ component: trace.get().component })
-    error(
-      'shadowbind_no_shadow_root',
-      'Subscribed web component has no shadowRoot. Be sure to call ' +
-        "this.attachShadow({ mode: open }) in the component's constructor"
-    )
-  }
+  if (!component.shadowRoot) shadowRootError(component)
 
   recursiveWalk(component.shadowRoot)
+}
+
+function shadowRootError (component) {
+  try {
+    component.attachShadow({ mode: 'open' })
+  } catch (err) {
+    trace.set({ component: trace.get().component })
+    error(
+      'shadowbind_closed_shadow_root',
+      'Subscribed component has a closed shadowRoot, but only open ' +
+        'shadowRoots are supported'
+    )
+  }
+  trace.set({ component: trace.get().component })
+  error(
+    'shadowbind_no_shadow_root',
+    'Subscribed web component has no shadowRoot. Be sure to call ' +
+      "this.attachShadow({ mode: open }) in the component's constructor"
+  )
 }
