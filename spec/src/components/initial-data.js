55fdf30347dd0a521de254bd9cadb26a84bd094f
Alexander Flenniken <Alexander@MacBook-Pro.hsd1.wa.comcast.net>
2018-04-18 22:07:10 -0700

    Add tests for two release-blocking issues

---
+++ spec/src/components/initial-data.js
@@ -0,0 +1,51 @@
+import Shadowbind from '../../../src/index.js'
+
+class InitialData extends window.HTMLElement {
+  getActual () {
+    let tests = []
+    Shadowbind.publish({ initial: '100% awesome' })
+    this.data({ inner: 'initial-data-state' })
+    tests.push(this.innerContent())
+    this.data({ inner: 'initial-data-attr' })
+    tests.push(this.innerContent())
+    return tests
+  }
+  getExpected () {
+    return ['100% awesome', '101% awesome']
+  }
+  innerContent () {
+    const testElement = this.shadowRoot.querySelector('#inner')
+    return testElement.shadowRoot.querySelector('h1').innerText
+  }
+  template () {
+    return /* @html */`
+      <span :tag="inner" detect-attr="101% awesome" id="inner"></span>
+    `
+  }
+}
+
+class InitialDataState extends window.HTMLElement {
+  subscribe () {
+    return { message: { state: 'initial' } }
+  }
+  template () {
+    return /* @html */`
+      <h1 :text="message">Loading from state...</h1>
+    `
+  }
+}
+
+class InitialDataAttr extends window.HTMLElement {
+  subscribe () {
+    return { message: { attr: 'detectAttr' } }
+  }
+  template () {
+    return /* @html */`
+      <h1 :text="message">Loading from attr...</h1>
+    `
+  }
+}
+
+Shadowbind.define(InitialData)
+Shadowbind.define(InitialDataState)
+Shadowbind.define(InitialDataAttr)
---
+++ spec/src/components/initial-events.js
@@ -0,0 +1,23 @@
+import Shadowbind from '../../../src/index.js'
+
+class InitialEvents extends window.HTMLElement {
+  getActual () {
+    this.eventTriggered = false
+    const button = this.shadowRoot.querySelector('button')
+    button.dispatchEvent(new window.Event('click'))
+    return this.eventTriggered
+  }
+  getExpected () {
+    return true
+  }
+  increment () {
+    this.eventTriggered = true
+  }
+  template () {
+    return /* @html */`
+      <button on:click="increment">Increment</button>
+    `
+  }
+}
+
+Shadowbind.define(InitialEvents)
