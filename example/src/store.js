52786e1034cf4b02a1781cca6ab9d217bbe3f090
Alexander Flenniken <Alexander@MacBook-Pro.hsd1.wa.comcast.net>
2018-04-21 16:27:34 -0700

    Refine store code in example

--- example/src/store.js
+++ example/src/store.js
@@ -3,8 +3,6 @@ import Shadowbind from '../../dist/shadowbind'
 import counter from './reducers/counter'
 
 const rootReducer = combineReducers({ counter })
+const middleware = applyMiddleware(Shadowbind.redux)
 
-export default createStore(
-  rootReducer,
-  applyMiddleware(Shadowbind.redux)
-)
+export default createStore(rootReducer, middleware)
